6071025f4c7aa3426f2b315732cc8422fa745974
bsheepy <bsheepy.hf@gmail.com>
2015-10-11 22:18:06 +0100

    plot 2 added

--- new figures/plot2.R
+++ new figures/plot2.R
@@ -5,24 +5,25 @@
 #
 
 # download data
+if(!dir.exists("./data")){dir.create("./data")}
+dataURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(dataURL, "./data/epc.zip")
 
-# if(!dir.exists("./data")){dir.create("./data")}
-# dataURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-# download.file(dataURL, "./data/epc.zip")
-# 
-# # read it in
-# epcdata <- read.table(unz("./epc.zip", "household_power_consumption.txt"), header = TRUE, na.strings = "?", sep = ";")
-# 
-# # fix dates
-# epcdata$DateTime<-paste(epcdata$Date, epcdata$Time, sep = " ")
-# epcdata$DateTime<-strptime(epcdata$DateTime, format = "%d/%m/%Y %H:%M:%S")
-# epcdata$Date<-as.Date(epcdata$Date,"%d/%m/%Y")
-# 
-# # subset by dates
-# ndates<-as.Date(c("2007/02/01", "2007/02/02"), "%Y/%m/%d")
-# epcdata<-epcdata[epcdata$Date %in% ndates,]
+# read it in
+epcdata <- read.table(unz("./epc.zip", "household_power_consumption.txt"), header = TRUE, na.strings = "?", sep = ";")
+
+# fix dates
+epcdata$DateTime<-paste(epcdata$Date, epcdata$Time, sep = " ")
+epcdata$DateTime<-strptime(epcdata$DateTime, format = "%d/%m/%Y %H:%M:%S")
+epcdata$Date<-as.Date(epcdata$Date,"%d/%m/%Y")
+
+# subset by dates
+ndates<-as.Date(c("2007/02/01", "2007/02/02"), "%Y/%m/%d")
+epcdata<-epcdata[epcdata$Date %in% ndates,]
 
 # plot the graph
-
+plot(epcdata$DateTime, epcdata$Global_active_power, type = "n",xlab = "", ylab = "Global Active Power (kilowatts)")
 lines(epcdata$DateTime, epcdata$Global_active_power)
-title(main = "", ylab = "Global Active Power (kilowatts)")
+title(main = "")
+dev.copy(png, file = "plot2.png")
+dev.off()
